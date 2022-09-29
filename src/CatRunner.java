fad2c08169ff3c7c0164ed99beb57f06776f5b5f
ImamM5 <imamm@nycstudents.net>
2022-09-29 09:28:24 -0400

    new code

---
+++ src/CatRunner.java
@@ -0,0 +1,11 @@
+import java.io.CharArrayReader;
+
+public class CatRunner
+{
+    public static void main (String [] args)
+    {
+        Cat cat1 = new Cat("Ginger", 10, 50.4 );
+        cat1.printCatInfo();
+
+    }
+}
