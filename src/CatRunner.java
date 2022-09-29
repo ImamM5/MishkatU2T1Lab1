fc6532229025bd0efc7de0293e6c40743c7c7f20
ImamM5 <imamm@nycstudents.net>
2022-09-29 09:30:31 -0400

    new code

--- src/CatRunner.java
+++ src/CatRunner.java
@@ -5,7 +5,12 @@ public class CatRunner
     public static void main (String [] args)
     {
         Cat cat1 = new Cat("Ginger", 10, 50.4 );
+        cat1.introduce();
         cat1.printCatInfo();
+
+        Cat cat2 = new Cat("Java", 9, 20.6);
+        cat2.introduce();
+        cat2.printCatInfo();
 
     }
 }
